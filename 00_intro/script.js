4893020807755396a26ae09d71cd37f26cd1f295
IGNACIO JAVIER BORRAZ <95113020+ignacioborraz@users.noreply.github.com>
2024-08-19 22:48:12 -0300

    00_intro (#1)

---
+++ 00_intro/script.js
@@ -0,0 +1,5 @@
+const nombre = "ignacio"
+const nombreCompleto = nombre + " borraz"
+console.log(nombre);
+console.log(nombreCompleto);
+
---
+++ 00_intro/users/names.js
@@ -0,0 +1,5 @@
+const name1 = "igna"
+const name2 = "ana"
+const name3 = "juan"
+const names = [name1,name2,name3]
+console.log(names);
