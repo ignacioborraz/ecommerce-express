7b5ccea263e25579eb62d536ef9e00b1e4f6c0bc
igna <ignacioborraz@hotmail.com>
2024-09-11 21:43:24 -0300

    server

---
+++ server/src/data/products.manager.js
@@ -0,0 +1,65 @@
+import fs from "fs";
+import crypto from "crypto";
+
+class ProductsManager {
+  constructor(path) {
+    this.path = path;
+    this.exists();
+  }
+  exists() {
+    // 0. apenas se crea la instancia se tiene que verificar si existe o no existe el archivo
+    // si no existe, hay que crearlo con un array vacío
+    const exist = fs.existsSync(this.path);
+    if (!exist) {
+      fs.writeFileSync(this.path, JSON.stringify([]));
+      console.log("file created");
+    } else {
+      console.log("file already exists");
+    }
+  }
+  async readAll(category) {
+    try {
+      const data = await fs.promises.readFile(this.path, "utf-8");
+      const parseData = JSON.parse(data);
+      //console.log(parseData);
+      if (category) {
+        const filteredData = parseData.filter(
+          (each) => each.category === category
+        );
+        return filteredData;
+      } else {
+        return parseData;
+      }
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+  async read(id) {
+    try {
+      const all = await this.readAll();
+      const one = all.find((each) => each.id === id);
+      //console.log(one);
+      return one;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+  async create(data) {
+    try {
+      data.id = crypto.randomBytes(12).toString("hex");
+      const all = await this.readAll();
+      all.push(data);
+      const stringAll = JSON.stringify(all, null, 2);
+      await fs.promises.writeFile(this.path, stringAll);
+      return data.id;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+}
+
+const productsManager = new ProductsManager("./src/data/files/products.json");
+export default productsManager;
